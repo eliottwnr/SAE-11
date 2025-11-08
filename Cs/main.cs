6481d67a6b449e6001a4ffcbd6e9d4f10860b982
Eliott Wanner <eliott.wanner@gmail.com>
2025-11-08 15:57:39 +0100

    modifié nom de la classe main pour Reservations et ajouté structure Traversee

--- Cs/main.cs
+++ Cs/main.cs
@@ -1,6 +1,19 @@
 using System; 
 
-class main {
+class Reservations {
+    struct Traversee {
+        public uint liaison;
+        public uint[] date;
+        public uint[] heure;
+
+        public Traversee(uint l){
+            liaison = l;
+
+            date = new uint[3]; // Jour, Mois, Année 
+            heure = new uint[2]; // Heures, Minutes
+        }
+    }
+
     public static void Main(){
     }
 }
