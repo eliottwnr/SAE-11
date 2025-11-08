a21209b773b975041778dad62477abbb316e6d4e
Eliott Wanner <eliott.wanner@gmail.com>
2025-11-08 16:16:05 +0100

    ajouté méthode afficherLiaison

--- Cs/main.cs
+++ Cs/main.cs
@@ -42,7 +42,30 @@ class Reservations {
         return liaison;
     }
 
+    static void afficherLiaison(uint liaison){
+        switch (liaison){
+            case 1: 
+                Console.WriteLine("   Lorient\t-\tGroix");
+                break;
+                
+            case 2:
+                Console.WriteLine("   Groix\t-\tLorient");
+                break;
+
+            case 3: 
+                Console.WriteLine("   Quiberon\t-\tLe Palais");
+                break;
+
+            case 4: 
+                Console.WriteLine("   Le Palais\t-\tQuiberon");
+                break;
+
+            default: // rien besoin de mettre puisque liaison est forcément entre 1 et 4 (Vérifié lors de la saisie)
+                break;
+        }
+    }
+
     public static void Main(){
-        Console.WriteLine(saisirLiaison());
+        afficherLiaison(saisirLiaison());
     }
 }
