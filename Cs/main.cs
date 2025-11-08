313f8d6a9e483250c80fd47de6e4e0f0defc3736
Eliott Wanner <eliott.wanner@gmail.com>
2025-11-08 16:10:05 +0100

    ajouté méthode saisirLiaison

--- Cs/main.cs
+++ Cs/main.cs
@@ -14,6 +14,35 @@ class Reservations {
         }
     }
 
+    static uint saisirLiaison(){
+        uint liaison;
+        
+        string? saisie; // string? : string qui peut être null (au cas où aucune saisie)
+        bool saisieJuste;
+
+        Console.Clear();
+        Console.WriteLine("-- Choix de la liaison --\n\n");
+
+        Console.WriteLine("   Départ\t-\tArrivée\n");
+        Console.WriteLine("1. Lorient\t-\tGroix");
+        Console.WriteLine("2. Groix\t-\tLorient");
+        Console.WriteLine("3. Quiberon\t-\tLe Palais");
+        Console.WriteLine("4. Le Palais\t-\tQuiberon");
+
+        do {
+            Console.Write("--> ");
+            saisie = Console.ReadLine();
+            saisieJuste = uint.TryParse(saisie, out liaison);
+
+            if (!saisieJuste || (liaison != 1 && liaison != 2 && liaison != 3 && liaison != 4)){
+                Console.WriteLine("Saisie Incorrecte !");
+            }
+        } while (liaison != 1 && liaison != 2 && liaison != 3 && liaison != 4);
+
+        return liaison;
+    }
+
     public static void Main(){
+        Console.WriteLine(saisirLiaison());
     }
 }
