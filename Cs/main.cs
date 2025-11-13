8a928aa9faae35d7506e273886ce31b8a37fb050
Eliott Wanner <eliott.wanner@gmail.com>
2025-11-13 19:02:54 +0100

    ajout d'une traversée avec les paramètre saisis

--- Cs/main.cs
+++ Cs/main.cs
@@ -3,14 +3,16 @@ using System;
 // Programme principal
 partial class Programme { // partial permet de séparer en plusieurs fichiers une même classe
     public static void Main(){
-        uint[] date = saisirDate();
         string[] horairesDuJour;
-        uint[] horaire;
 
         Liaison liaison = saisirLiaison();
+
+        Traversee traversee = new Traversee(liaison);
         
+        traversee.date = saisirDate();
+
         // récupération des horaires du jour et de la liaison choisis
-        horairesJour(liaison, date[0], out horairesDuJour); // date[0] correspond au jour
-        horaire = saisirHoraire(horairesDuJour);
+        horairesJour(liaison, traversee.date[0], out horairesDuJour); // date[0] correspond au jour
+        traversee.heure = saisirHoraire(horairesDuJour);
     }
 }
