46aef178454e2d87a9124e27518535e62d4eb738
Eliott Wanner <eliott.wanner@gmail.com>
2025-11-19 19:48:57 +0100

    ajouté vérification de saisieJuste dans boucles des saisies

--- Cs/Saisie.cs
+++ Cs/Saisie.cs
@@ -13,13 +13,13 @@ partial class Programme {
 
         do {
             afficherPrompt();
-            saisie = Console.ReadLine();
-            saisieJuste = uint.TryParse(saisie, out liaison);
+            saisie = Console.ReadLine(); // peut renvoyer null (d'où string?)
+            saisieJuste = uint.TryParse(saisie, out liaison); // essaye de convertir la saisie en unsigned int, renvoie un booléen (succès ou échec)
 
             if (!saisieJuste || (liaison < 1 || liaison > 4)){
                 afficherSaisieIncorrecte();
             }
-        } while (liaison < 1 || liaison > 4);
+        } while (!saisieJuste || liaison < 1 || liaison > 4);
 
         return (Liaison) liaison; // cast (conversion) vers l'énumération
     }
@@ -42,7 +42,7 @@ partial class Programme {
             if (!saisieJuste || date[0] <= 0 || date[0] >= 31){
                 afficherSaisieIncorrecte();
             }
-        } while (date[0] <= 0 || date[0] >= 31); // tant que le jour n'est pas entre 1 et 30 (inclus)
+        } while (!saisieJuste || date[0] <= 0 || date[0] >= 31); // tant que le jour n'est pas entre 1 et 30 (inclus)
 
         do {
             Console.Write("Saisir le mois de la traversée : ");
@@ -52,7 +52,7 @@ partial class Programme {
             if (!saisieJuste || date[1] != 11){
                 afficherSaisieIncorrecte();
             }
-        } while (date[1] != 11); // tant que le mois n'est pas en novembre 
+        } while (!saisieJuste || date[1] != 11); // tant que le mois n'est pas en novembre 
 
         do {
             Console.Write("Saisir l'année de la traversée : ");
@@ -62,7 +62,7 @@ partial class Programme {
             if (!saisieJuste || date[2] != 2025){
                 afficherSaisieIncorrecte();
             }
-        } while (date[2] != 2025); // tant que le mois n'est pas 2025 
+        } while (!saisieJuste || date[2] != 2025); // tant que le mois n'est pas 2025 
 
         return date;
     }
@@ -83,10 +83,10 @@ partial class Programme {
             saisie = Console.ReadLine();
             saisieJuste = uint.TryParse(saisie, out horaireSaisie);
 
-            if (horaireSaisie <= 0 || horaireSaisie > nbHoraires){
+            if (!saisieJuste || horaireSaisie <= 0 || horaireSaisie > nbHoraires){
                 afficherSaisieIncorrecte();
             }
-        } while (horaireSaisie <= 0 || horaireSaisie > nbHoraires);
+        } while (!saisieJuste || horaireSaisie <= 0 || horaireSaisie > nbHoraires);
 
         heure[0] = uint.Parse(horaires[horaireSaisie - 1].Split(':')[0]); // sélectionne la première partie du bon horaire 
         heure[1] = uint.Parse(horaires[horaireSaisie - 1].Split(':')[1]); // sélectionne la seconde partie du bon horaire 
@@ -105,10 +105,10 @@ partial class Programme {
             saisie = Console.ReadLine();
             saisieJuste = uint.TryParse(saisie, out code);
 
-            if (code < 1 || code > 5){
+            if (!saisieJuste || code < 1 || code > 5){
                 afficherSaisieIncorrecte();
             }
-        } while (code < 1 || code > 5);
+        } while (!saisieJuste || code < 1 || code > 5);
 
         return (CodeCategoriePassager) (code - 1); // car les codes vont de 0 à 4 mais la saisie est de 1 à 5 (voir affichage)
     }
