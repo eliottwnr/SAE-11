6093bf25c307ab11f5a53be7b825da10a2e0627e
zolaski <57315647+zolaski333@users.noreply.github.com>
2025-12-03 23:40:08 +0100

    Remis ma version, car l'autre ne fonctionne pas

--- Cs/CSV/RecupCSV.cs
+++ Cs/CSV/RecupCSV.cs
@@ -1,16 +1,20 @@
 using System;
 using System.IO;
+using System.Globalization;
 
 // Gère la récupération des données dans le csv correspondant
-partial class Programme {
+partial class Programme
+{
     // renvoie true si la lecture s'est bien passée, false sinon
-    static bool lireFichier(string cheminAccess, out string[] lignes){
+    static bool lireFichier(string cheminAccess, out string[] lignes)
+    {
         bool estLue = false;
 
         long nbLignes = 0; // car newFileInfo.length renvoie un long
         lignes = new string[nbLignes];
 
-        if (File.Exists(cheminAccess)){
+        if (File.Exists(cheminAccess))
+        {
             nbLignes = new System.IO.FileInfo(cheminAccess).Length; // récupère le nombre de lignes du fichier
             lignes = new string[nbLignes]; // chaque élément du tableau est une ligne du fichier
 
@@ -22,13 +26,15 @@ partial class Programme {
         return estLue;
     }
 
-    static bool horairesJour(Liaison liaison, uint jour, out string[] horaires){ // jour étant le jour du mois
+    static bool horairesJour(Liaison liaison, uint jour, out string[] horaires)
+    { // jour étant le jour du mois
         bool succes = false;
         string cheminAccess = "CSV/Horaires/";
 
         horaires = new string[0]; // initialise un tableau vide au cas où la fonction lireFichier n'arrive pas à lire
 
-        switch (liaison){
+        switch (liaison)
+        {
             case Liaison.lorient_groix:
                 cheminAccess += "lorient_groix.csv";
                 break;
@@ -51,7 +57,8 @@ partial class Programme {
         return succes;
     }
 
-    static bool tarifPassager(CodeCategoriePassager passager, uint liaison, out double tarif){
+    static bool tarifPassager(CodeCategoriePassager passager, uint liaison, out double tarif)
+    {
         bool succes = false;
 
         string cheminAccess = "CSV/Tarifs/passagers.csv"; // de forme numéro du CodeCategoriePassager;tarifGroix-Lorient;tarifLePalais-Quiberon
@@ -60,47 +67,55 @@ partial class Programme {
         string[] tarifs;
         string[] ligne;
 
-        uint indiceLiaison = 0; // 0 pour groix / lorient et 1 pour le palais / quiberon
+        uint indiceLiaison = 1; // 0 pour groix / lorient et 1 pour le palais / quiberon
 
-        if (liaison <= 2){ // groix-lorient et lorient-groix
-            indiceLiaison = 0;
+        if (liaison <= 2)
+        { // groix-lorient et lorient-groix
+            indiceLiaison = 2;
         }
-        else { // lepalais-quiberon et quiberon-lepalais
+        else
+        { // lepalais-quiberon et quiberon-lepalais
             indiceLiaison = 1;
         }
 
         succes = lireFichier(cheminAccess, out tarifs);
         nbLignesFichier = tarifs.Length;
 
-        ligne = tarifs[(int) passager].Split(';'); 
+        ligne = tarifs[(int)passager].Split(';');
 
-        tarif = double.Parse(ligne[indiceLiaison]); // sélectionne le bon tarif de la ligne 
+        if (ligne.Length <= indiceLiaison)
+        {
+            tarif = 0.0;
+            return false;
+        }
+
+        tarif = double.Parse(ligne[indiceLiaison], CultureInfo.InvariantCulture); // sélectionne le bon tarif de la ligne 
 
         return succes;
     }
 
-    static bool tarifVehicule(CodeCategorieVehicule vehicule, uint liaison, out double tarif){
+    static bool tarifVehicule(CodeCategorieVehicule vehicule, uint liaison, out double tarif)
+    {
         bool succes = false;
-
-        string cheminAccess = "CSV/Tarifs/vehicules.csv"; // de forme numéro du CodeCategorieVehicule;tarifGroix-Lorient;tarifLePalais-Quiberon
-        int nbLignesFichier = 0;
-
+        tarif = 0;
+        string cheminAccess = "CSV/Tarifs/vehicules.csv";
         string[] tarifs;
-        string[] ligne;
-
-        uint indiceLiaison = 0; // 0 pour groix / lorient et 1 pour le palais / quiberon
-
-        if (liaison > 2){ 
-            indiceLiaison = 1;
-        }
 
         succes = lireFichier(cheminAccess, out tarifs);
-        nbLignesFichier = tarifs.Length;
+        if (!succes) return false;
 
-        ligne = tarifs[(int) vehicule].Split(';'); 
+        int indexLigne = (int)vehicule;
 
-        tarif = double.Parse(ligne[indiceLiaison]); // sélectionne le bon tarif de la ligne 
+        string[] ligne = tarifs[indexLigne].Split(';');
 
-        return succes;
+        uint indiceLiaison = 0; // vers Groix
+        if (liaison > 2)
+        {
+            indiceLiaison = 1; // vers Le Palais
+        }
+
+        tarif = double.Parse(ligne[indiceLiaison], CultureInfo.InvariantCulture);
+
+        return true;
     }
 }
