312ea2098898b96cbc6ca8899fe3d97ab6c2eb1e
Eliott Wanner <eliott.wanner@gmail.com>
2025-12-03 23:21:55 +0100

    restauré fichier car fonctionnel et supprimé l'espace en trop dans le csv

--- Cs/CSV/RecupCSV.cs
+++ Cs/CSV/RecupCSV.cs
@@ -1,20 +1,16 @@
 using System;
 using System.IO;
-using System.Globalization;
 
 // Gère la récupération des données dans le csv correspondant
-partial class Programme
-{
+partial class Programme {
     // renvoie true si la lecture s'est bien passée, false sinon
-    static bool lireFichier(string cheminAccess, out string[] lignes)
-    {
+    static bool lireFichier(string cheminAccess, out string[] lignes){
         bool estLue = false;
 
         long nbLignes = 0; // car newFileInfo.length renvoie un long
         lignes = new string[nbLignes];
 
-        if (File.Exists(cheminAccess))
-        {
+        if (File.Exists(cheminAccess)){
             nbLignes = new System.IO.FileInfo(cheminAccess).Length; // récupère le nombre de lignes du fichier
             lignes = new string[nbLignes]; // chaque élément du tableau est une ligne du fichier
 
@@ -26,15 +22,13 @@ partial class Programme
         return estLue;
     }
 
-    static bool horairesJour(Liaison liaison, uint jour, out string[] horaires)
-    { // jour étant le jour du mois
+    static bool horairesJour(Liaison liaison, uint jour, out string[] horaires){ // jour étant le jour du mois
         bool succes = false;
         string cheminAccess = "CSV/Horaires/";
 
         horaires = new string[0]; // initialise un tableau vide au cas où la fonction lireFichier n'arrive pas à lire
 
-        switch (liaison)
-        {
+        switch (liaison){
             case Liaison.lorient_groix:
                 cheminAccess += "lorient_groix.csv";
                 break;
@@ -57,8 +51,7 @@ partial class Programme
         return succes;
     }
 
-    static bool tarifPassager(CodeCategoriePassager passager, uint liaison, out double tarif)
-    {
+    static bool tarifPassager(CodeCategoriePassager passager, uint liaison, out double tarif){
         bool succes = false;
 
         string cheminAccess = "CSV/Tarifs/passagers.csv"; // de forme numéro du CodeCategoriePassager;tarifGroix-Lorient;tarifLePalais-Quiberon
@@ -67,55 +60,47 @@ partial class Programme
         string[] tarifs;
         string[] ligne;
 
-        uint indiceLiaison = 1; // 0 pour groix / lorient et 1 pour le palais / quiberon
+        uint indiceLiaison = 0; // 0 pour groix / lorient et 1 pour le palais / quiberon
 
-        if (liaison <= 2)
-        { // groix-lorient et lorient-groix
-            indiceLiaison = 2;
+        if (liaison <= 2){ // groix-lorient et lorient-groix
+            indiceLiaison = 0;
         }
-        else
-        { // lepalais-quiberon et quiberon-lepalais
+        else { // lepalais-quiberon et quiberon-lepalais
             indiceLiaison = 1;
         }
 
         succes = lireFichier(cheminAccess, out tarifs);
         nbLignesFichier = tarifs.Length;
 
-        ligne = tarifs[(int)passager].Split(';');
+        ligne = tarifs[(int) passager].Split(';'); 
 
-        if (ligne.Length <= indiceLiaison)
-        {
-            tarif = 0.0;
-            return false;
-        }
-
-        tarif = double.Parse(ligne[indiceLiaison], CultureInfo.InvariantCulture); // sélectionne le bon tarif de la ligne 
+        tarif = double.Parse(ligne[indiceLiaison]); // sélectionne le bon tarif de la ligne 
 
         return succes;
     }
 
-    static bool tarifVehicule(CodeCategorieVehicule vehicule, uint liaison, out double tarif)
-    {
+    static bool tarifVehicule(CodeCategorieVehicule vehicule, uint liaison, out double tarif){
         bool succes = false;
-        tarif = 0;
-        string cheminAccess = "CSV/Tarifs/vehicules.csv";
+
+        string cheminAccess = "CSV/Tarifs/vehicules.csv"; // de forme numéro du CodeCategorieVehicule;tarifGroix-Lorient;tarifLePalais-Quiberon
+        int nbLignesFichier = 0;
+
         string[] tarifs;
+        string[] ligne;
 
-        succes = lireFichier(cheminAccess, out tarifs);
-        if (!succes) return false;
+        uint indiceLiaison = 0; // 0 pour groix / lorient et 1 pour le palais / quiberon
 
-        int indexLigne = (int)vehicule;
-
-        string[] ligne = tarifs[indexLigne].Split(';');
-
-        uint indiceLiaison = 0; // vers Groix
-        if (liaison > 2)
-        {
-            indiceLiaison = 1; // vers Le Palais
+        if (liaison > 2){ 
+            indiceLiaison = 1;
         }
 
-        tarif = double.Parse(ligne[indiceLiaison], CultureInfo.InvariantCulture);
+        succes = lireFichier(cheminAccess, out tarifs);
+        nbLignesFichier = tarifs.Length;
 
-        return true;
+        ligne = tarifs[(int) vehicule].Split(';'); 
+
+        tarif = double.Parse(ligne[indiceLiaison]); // sélectionne le bon tarif de la ligne 
+
+        return succes;
     }
 }
